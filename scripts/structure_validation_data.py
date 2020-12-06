6f64e8af9f13372fecedbeb4c4a9190e08497322
Ivan Valles Perez <ivanvallesperez@gmail.com>
2020-12-06 19:37:37 +0000

    Fix tf class mapping

--- scripts/structure_validation_data.py
+++ scripts/structure_validation_data.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import argparse
 import os
+import requests
+import numpy as np
 from tqdm import tqdm
+from scipy.io import loadmat
+
+
+TF_META_URL = "https://storage.googleapis.com/download.tensorflow.org/data/imagenet_class_index.json"
 
 
 def parse_args():
@@ -24,15 +30,40 @@ def parse_args():
         required=True,
         help="Path of the file containing the validation data index",
     )
+    argparser.add_argument(
+        "-m",
+        "--meta-path",
+        required=True,
+        help="Meta info path (with .mat extension) provided with Imagenet.",
+    )
+
     return argparser.parse_args()
+
+
+def get_imagenet_tf_mapping(meta_path):
+    tf_json = requests.get(TF_META_URL).json()
+    imagenet_meta = imagenet_meta = loadmat(meta_path)
+    ids = np.array(list(map(lambda x: x[0][0], imagenet_meta["synsets"]))).squeeze()
+    codes = np.array(list(map(lambda x: x[0][1], imagenet_meta["synsets"]))).squeeze()
+    imagenet_json = dict(zip(codes, ids))
+
+    mapping = {}
+    for key_tf, val_tf in tf_json.items():
+        tf_code, tf_desc = val_tf
+        imagenet_id = imagenet_json[tf_code]
+        mapping[int(imagenet_id)] = int(key_tf)
+    return mapping
 
 
 def generate_validation_data_structure():
     args = parse_args()
 
     # Load the index file, that maps from image number to class number
-    index = open(args.index_path, "r").readlines()
+    index = map(lambda x: int(x), open(args.index_path, "r").readlines())
 
+    # Map imagenet indices to tensorflow class id
+    tf_mapping = get_imagenet_tf_mapping(args.meta_path)
+    index = list(map(tf_mapping.get, index))
     filename_template = "ILSVRC2012_val_{:08d}.JPEG"
     os.makedirs(args.output_path, exist_ok=True)
     for i in tqdm(range(50000)):  # Number of validation images at ImageNet
