676405f35db3d3f4e4d16a89733282f00e900615
Ivan Valles Perez <ivanvallesperez@gmail.com>
2020-12-06 12:35:29 +0000

    Add type parameter to select between jpg and npz

--- scripts/preprocess_validation_data.py
+++ scripts/preprocess_validation_data.py
@@ -20,6 +20,7 @@ def parse_args():
         required=True,
         help="Size of the output images",
     )
+    argparser.add_argument("-t", "--type", default="jpeg", help="'jpeg' or 'npz'")
     return argparser.parse_args()
 
 
@@ -43,6 +44,7 @@ def preprocess(img, size):
 
 def process_images():
     args = parse_args()
+    type_ = args.type
     size = int(args.size)
     input_paths = glob.glob(os.path.join(args.input_path, "*", "*.JPEG"))
     output_dir = f"{os.path.normpath(args.input_path)}_{args.size}"
@@ -53,15 +55,24 @@ def process_images():
 
         img = preprocess(img, size)
         assert img.size == (size, size)
-        
-        # Convert to numpy and save to npz
-        img_np = np.array(img)
+
         class_name = os.path.split(os.path.split(input_path)[0])[1]
-        file_name = os.path.splitext(os.path.split(input_path)[1])[0] + ".npz"
         parent_folder = os.path.join(output_dir, class_name)
         os.makedirs(parent_folder, exist_ok=True)
-        output_path = os.path.join(parent_folder, file_name)
-        np.savez(output_path, img=img_np)
+
+        if type_ == "jpeg":
+            file_name = os.path.splitext(os.path.split(input_path)[1])[0] + ".JPEG"
+            output_path = os.path.join(parent_folder, file_name)
+            img.save(output_path)
+        elif type_ == "npz":  # Convert to numpy and save to npz
+            img_np = np.array(img)
+            file_name = os.path.splitext(os.path.split(input_path)[1])[0] + ".npz"
+            output_path = os.path.join(parent_folder, file_name)
+            np.savez(output_path, img=img_np)
+        else:
+            raise ValueError(
+                f"Type '{type_}' not recognized as a valid type (jpeg or npz)."
+            )
 
 
 if __name__ == "__main__":
