40e11aa535d7289f92374e115296c82b6620b5ce
Ivan Valles Perez <ivanvallesperez@gmail.com>
2020-12-08 22:51:55 +0000

    Implement multiprocessing into data processor

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import math
 import glob
+from multiprocessing.dummy import Pool
+from multiprocessing import cpu_count
 from tqdm import tqdm
 from PIL import Image
 import argparse
@@ -21,7 +23,7 @@ def parse_args():
         required=True,
         help="Size of the output images",
     )
-    argparser.add_argument("-t", "--type", default="jpeg", help="'jpeg' or 'npz'")
+    argparser.add_argument("-j", "--n-jobs", default=-1, help="Number of threads")
     return argparser.parse_args()
 
 
@@ -41,39 +43,41 @@ def preprocess(img, size):
     img = img.crop((left, top, right, bottom))
     return img
 
+def load_process_and_save(input_path, output_dir, size):
+    input_dir = os.path.split(os.path.split(input_path)[0])[0]
+
+    img = Image.open(input_path)
+
+    img = preprocess(img, size)
+    assert img.size == (size, size)
+
+    class_name = os.path.split(os.path.split(input_path)[0])[1]
+    parent_folder = os.path.join(output_dir, class_name)
+    os.makedirs(parent_folder, exist_ok=True)
+
+    file_name = os.path.splitext(os.path.split(input_path)[1])[0] + ".JPEG"
+    output_path = os.path.join(parent_folder, file_name)
+    try:
+        img.save(output_path, quality=100, subsampling=0)
+    except OSError:
+        img = img.convert("RGB")
+        img.save(output_path, quality=100, subsampling=0)
 
 def process_images():
     args = parse_args()
-    type_ = args.type
     size = int(args.size)
+    n_jobs = int(args.n_jobs) if int(args.n_jobs) != -1 else cpu_count()
+
     input_paths = glob.glob(os.path.join(args.input_path, "*", "*.JPEG"))
     output_dir = f"{os.path.normpath(args.input_path)}_{args.size}"
     os.makedirs(output_dir, exist_ok=True)
-    for input_path in tqdm(input_paths):
 
-        img = Image.open(input_path)
-
-        img = preprocess(img, size)
-        assert img.size == (size, size)
-
-        class_name = os.path.split(os.path.split(input_path)[0])[1]
-        parent_folder = os.path.join(output_dir, class_name)
-        os.makedirs(parent_folder, exist_ok=True)
-
-        if type_ == "jpeg":
-            file_name = os.path.splitext(os.path.split(input_path)[1])[0] + ".JPEG"
-            output_path = os.path.join(parent_folder, file_name)
-            img.save(output_path, quality=100, subsampling=0)
-        elif type_ == "npz":  # Convert to numpy and save to npz
-            img_np = np.array(img)
-            file_name = os.path.splitext(os.path.split(input_path)[1])[0] + ".npz"
-            output_path = os.path.join(parent_folder, file_name)
-            np.savez(output_path, img=img_np)
-        else:
-            raise ValueError(
-                f"Type '{type_}' not recognized as a valid type (jpeg or npz)."
-            )
-
+    pool = Pool(n_jobs)
+    p=pool.imap(lambda x: load_process_and_save(x, output_dir, size), input_paths)
+    for _ in tqdm(p, total=len(input_paths)):
+        pass
+    pool.close()
+    pool.join()
 
 if __name__ == "__main__":
     process_images()
