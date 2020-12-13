4cbe7c177c5183c619dc379968cdf4fc527dcb6a
Ivan Valles Perez <ivanvallesperez@gmail.com>
2020-12-13 23:50:40 +0000

    Add more paths

--- src/paths.py
+++ src/paths.py
@@ -3,3 +3,21 @@ import os
 
 def get_dataset_path():
     return os.path.join("/mnt", "FLASH", "datasets", "imagenet")
+
+
+def get_model_path(alias):
+    path = os.path.join(get_models_path(), alias)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+
+def get_logs_path(alias):
+    path = os.path.join(get_model_path(alias), "logs")
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+
+def get_models_path():
+    return "./models"
