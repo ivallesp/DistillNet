6ae3e48be9482f5d0efe7034c26d88c70b560745
Ivan Valles Perez <ivanvallesperez@gmail.com>
2020-12-26 20:17:40 +0000

    Refactor training script

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import math
 import os
 import argparse
 from keras import Model
+from functools import partial
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 from src.model import (
@@ -17,6 +18,58 @@ import tensorflow as tf
 
 BATCH_SIZE = 35
 PROB_NORM = 0.35
+
+
+class TBCallbackMod(tf.keras.callbacks.TensorBoard):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.train_started = False
+
+    def on_train_end(self, logs=None):
+        # Do not close the writers when train ends
+        pass
+
+    def on_train_begin(self, logs=None):
+        # Do instantiate the writer and global step only the first time
+        if not self.train_started:
+            self._global_train_batch = 0
+            self._push_writer(self._train_writer, self._train_step)
+            self.train_started = True
+
+
+def get_training_generator(
+    transfer_dataset_path,
+    preprocess_input,
+    size,
+    teachers_combination_method,
+    batch_size,
+):
+    transfer_dataset_alias = os.path.split(transfer_dataset_path)[1].split("_")[0]
+    unlabeled = True if transfer_dataset_alias == "test" else False
+
+    # Build soft-targets
+    soft_targets = get_combined_soft_targets(
+        models=MODELS_CATALOG,
+        dataset=transfer_dataset_alias,
+        prob1=PROB_NORM,
+        method=teachers_combination_method,
+    )
+
+    # Build train data generator
+    idg_train = ImageDataGenerator(preprocessing_function=preprocess_input)
+    flow_train = idg_train.flow_from_directory(
+        transfer_dataset_path,
+        shuffle=True,
+        target_size=(size, size),
+        batch_size=batch_size,
+    )
+
+    gen_train = get_data_generator_with_soft_targets(
+        flow_train, soft_targets, unlabeled
+    )
+
+    steps_per_epoch = math.ceil(flow_train.samples / flow_train.batch_size)
+    return gen_train, steps_per_epoch
 
 
 def parse_args():
@@ -58,8 +111,6 @@ def train_model(base_model_name, teachers_combination_method, random_seed):
     data_path = get_dataset_path()
     validation_dataset_path = os.path.join(data_path, f"validation_{size}")
     transfer_dataset_path = os.path.join(data_path, f"test_{size}")
-    transfer_dataset_alias = os.path.split(transfer_dataset_path)[1].split("_")[0]
-    unlabeled = True if transfer_dataset_alias == "test" else False
 
     # Alias name generation
     model_alias = f"{base_model_name}-{teachers_combination_method}-{random_seed}"
@@ -69,44 +120,25 @@ def train_model(base_model_name, teachers_combination_method, random_seed):
     output = keras.backend.softmax(model.output / T)
     model = Model(input_, output)
 
-    # Build soft-targets
-    soft_targets = get_combined_soft_targets(
-        models=MODELS_CATALOG,
-        dataset=transfer_dataset_alias,
-        prob1=PROB_NORM,
-        method=teachers_combination_method,
-    )
+    # Define top5 accuracy metric
+    top5_acc = partial(tf.keras.metrics.top_k_categorical_accuracy, k=5)
+    top5_acc.__name__ = "top5_accuracy"
 
-    # Build train data generator
-    idg_train = ImageDataGenerator(preprocessing_function=preprocess_input)
-    flow_train = idg_train.flow_from_directory(
-        transfer_dataset_path,
-        shuffle=True,
-        target_size=(size, size),
-        BATCH_SIZE=BATCH_SIZE,
-    )
-    gen_train = get_data_generator_with_soft_targets(
-        flow_train, soft_targets, unlabeled
-    )
-
-    # Build validation data generator
-    idg_val = ImageDataGenerator(preprocessing_function=preprocess_input)
-    flow_val = idg_val.flow_from_directory(
-        validation_dataset_path,
-        shuffle=True,
-        target_size=(size, size),
-        BATCH_SIZE=BATCH_SIZE,
-    )
-
+    # Compile the model
     model.compile(
-        Adam(learning_rate=1e-5), "categorical_crossentropy", metrics=["accuracy"]
+        Adam(learning_rate=1e-5),
+        "categorical_crossentropy",
+        metrics=["accuracy", top5_acc],
     )
 
-    print("Running initial validation...")
-    model.evaluate(flow_val)
-    print("Running train...")
-    tb_callback = tf.keras.callbacks.TensorBoard(
-        get_logs_path(model_alias), update_freq=100
+    # Build callbacks
+    if teachers_combination_method == "random":
+        TBCallback = TBCallbackMod
+    else:
+        TBCallback = tf.keras.callbacks.TensorBoard
+    tb_callback = TBCallback(
+        get_logs_path(model_alias),
+        update_freq=100,
     )
     cp_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=os.path.join(
@@ -116,13 +148,60 @@ def train_model(base_model_name, teachers_combination_method, random_seed):
         verbose=1,
         save_weights_only=True,
     )
-    model.fit(
-        gen_train,
-        steps_per_epoch=math.ceil(flow_train.samples / flow_train.BATCH_SIZE),
-        validation_data=flow_val,
-        epochs=1000,
-        callbacks=[tb_callback, cp_callback],
+
+    # Build validation data generator
+    idg_val = ImageDataGenerator(preprocessing_function=preprocess_input)
+    flow_val = idg_val.flow_from_directory(
+        validation_dataset_path,
+        shuffle=True,
+        target_size=(size, size),
+        batch_size=BATCH_SIZE,
     )
+    # Running baseline evaluation against validation data
+    print("Running initial validation...")
+    model.evaluate(flow_val)
+
+    if teachers_combination_method == "random":
+        # Update the random teachers every epoch
+        for epoch in range(100):
+            gen_train, steps_per_epoch = get_training_generator(
+                transfer_dataset_path=transfer_dataset_path,
+                preprocess_input=preprocess_input,
+                size=size,
+                teachers_combination_method=teachers_combination_method,
+                batch_size=BATCH_SIZE,
+            )
+            model.fit(
+                gen_train,
+                steps_per_epoch=steps_per_epoch,
+                validation_data=flow_val,
+                epochs=epoch + 1,
+                initial_epoch=epoch,
+                callbacks=[tb_callback, cp_callback],
+            )
+        # Manually close the writer
+        tb_callback._pop_writer()
+        if tb_callback._is_tracing:
+            tb_callback._stop_trace()
+
+        tb_callback._close_writers()
+        tb_callback._delete_tmp_write_dir()
+    else:
+        gen_train, steps_per_epoch = get_training_generator(
+            transfer_dataset_path=transfer_dataset_path,
+            preprocess_input=preprocess_input,
+            size=size,
+            teachers_combination_method=teachers_combination_method,
+            batch_size=BATCH_SIZE,
+        )
+
+        model.fit(
+            gen_train,
+            steps_per_epoch=steps_per_epoch,
+            validation_data=flow_val,
+            epochs=1000,
+            callbacks=[tb_callback, cp_callback],
+        )
 
 
 if __name__ == "__main__":
